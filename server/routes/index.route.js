c01ad6d3683e499747e521078f7e5e486a668b08
Joy <joymridha939@gmail.com>
2024-05-11 00:49:17 +0530

    done register page design

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -6,6 +6,10 @@ router.get("/login", (req, res) => {
 	res.render("login");
 });
 
+router.get("/register", (req, res) => {
+	res.render("register");
+});
+
 router.get("/messages/:id", messageController);
 
 export default router;
