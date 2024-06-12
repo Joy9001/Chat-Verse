70a860ebe4a1e862de43235c37848173dcd3d4ee
Joy <joymridha939@gmail.com>
2024-06-12 23:07:27 +0530

    using adventurer avatars

--- server/helpers/generateAvatar.helper.js
+++ server/helpers/generateAvatar.helper.js
@@ -2,7 +2,7 @@ import { createAvatar } from '@dicebear/core'
 import * as avatars from '@dicebear/collection'
 
 export const generateAvatar = (name) => {
-    const avatar = createAvatar(avatars.micah, {
+    const avatar = createAvatar(avatars.adventurer, {
         seed: name,
     })
 
