22b1040eb096c70b93ceeed6b488f504d04a0241
Joy <joymridha939@gmail.com>
2024-08-09 17:07:11 +0530

    chore: Update rate limit to allow only 20 requests per IP within 5 minutes

--- server/helpers/rateLimit.helper.js
+++ server/helpers/rateLimit.helper.js
@@ -2,7 +2,7 @@ import { rateLimit } from 'express-rate-limit'
 
 const limiter = rateLimit({
     windowMs: 5 * 60 * 1000,
-    limit: 100,
+    limit: 20,
     message: 'Too many requests from this IP, please try again after 5 minutes',
 })
 
