ae0834cc12eae9e1413f139bc57b70b53f20073f
Joy Mridha <joymridha939@gmail.com>
2025-04-28 00:41:13 +0530

    Initialize user authentication on app load
    - Added useEffect to fetch the current user when the App component mounts.
    - Integrated useAuthStore to manage user state effectively.

--- client-react/src/App.tsx
+++ client-react/src/App.tsx
@@ -1,7 +1,13 @@
 import { Outlet } from '@tanstack/react-router'
+import { useEffect } from 'react'
 import './App.css'
+import { useAuthStore } from './store/auth.store'
 
 function App() {
+	useEffect(() => {
+		useAuthStore.getState().fetchCurrentUser()
+	}, [])
+
 	return (
 		<div className="min-h-screen bg-background font-dm">
 			<Outlet /> {/* This will render the matched route */}
