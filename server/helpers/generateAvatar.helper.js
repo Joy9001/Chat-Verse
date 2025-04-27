7b7f9b1218fc067eb85c1b34b0b3b4545768c599
Joy Mridha <joymridha939@gmail.com>
2025-04-27 14:39:20 +0530

    Refactor avatar generation helper
    - Change `toDataUriSync` to `toDataUri` in `generateAvatar` function for improved performance

--- server/helpers/generateAvatar.helper.js
+++ server/helpers/generateAvatar.helper.js
@@ -1,22 +1,22 @@
-import { createAvatar } from '@dicebear/core'
-import * as avatars from '@dicebear/collection'
+import * as avatars from "@dicebear/collection";
+import { createAvatar } from "@dicebear/core";
 
 const generateAvatar = () => {
-	const avatar = createAvatar(avatars.adventurer, {
-		seed: Math.floor(Math.random() * 10000),
-	})
+  const avatar = createAvatar(avatars.adventurer, {
+    seed: Math.floor(Math.random() * 10000),
+  });
 
-	const dataUri = avatar.toDataUriSync()
-	return dataUri
-}
+  const dataUri = avatar.toDataUri();
+  return dataUri;
+};
 
 const generateGroupAvatar = () => {
-	const groupAvatar = createAvatar(avatars.thumbs, {
-		seed: Math.floor(Math.random() * 10000),
-	})
+  const groupAvatar = createAvatar(avatars.thumbs, {
+    seed: Math.floor(Math.random() * 10000),
+  });
 
-	const dataUri = groupAvatar.toDataUriSync()
-	return dataUri
-}
+  const dataUri = groupAvatar.toDataUriSync();
+  return dataUri;
+};
 
-export { generateAvatar, generateGroupAvatar }
+export { generateAvatar, generateGroupAvatar };
