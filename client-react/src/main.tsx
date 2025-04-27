bd6777ba91b1a40aa3499fad4b8b94467973187a
Joy Mridha <joymridha939@gmail.com>
2025-04-28 00:41:07 +0530

    Add React Query and Toaster integration in main.tsx
    - Introduced QueryClient and QueryClientProvider for managing server state with React Query.
    - Integrated Toaster component for enhanced user notifications.
    - Updated the root render method to include the new providers, ensuring proper context for the application.

--- client-react/src/main.tsx
+++ client-react/src/main.tsx
@@ -1,8 +1,13 @@
+import { Toaster } from "@/components/ui/sonner"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { router } from './routes'
+
+// Create a client
+const queryClient = new QueryClient()
 
 // Initialize the router
 declare module '@tanstack/react-router' {
@@ -16,7 +21,10 @@ if (!rootElement.innerHTML) {
   const root = createRoot(rootElement)
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <Toaster richColors closeButton />
+      </QueryClientProvider>
     </StrictMode>,
   )
 }
