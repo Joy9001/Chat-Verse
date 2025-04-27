3e82540ca44cb6b1a6a69734efbfe723604aa09c
Joy Mridha <joymridha939@gmail.com>
2025-04-28 00:40:23 +0530

    Improve JWT strategy user retrieval and logging
    - Updated user retrieval in the JWT strategy to exclude the password field for enhanced security.
    - Added console logging for cases where a user is not found, improving debugging capabilities.
    - Simplified user session assignment to directly use the user object.

--- server/strategies/passport-jwt.strategy.js
+++ server/strategies/passport-jwt.strategy.js
@@ -26,14 +26,14 @@ const options = {
 export default passport.use(
 	new JwtStrategy(options, async (req, jwt_payload, done) => {
 		try {
-			let user = await User.findById(jwt_payload.user);
-			if (!user) return done(null, false);
+			let user = await User.findById(jwt_payload.user).select('-password');
+			if (!user) {
+				console.log(`JWT Strategy: User not found for ID ${jwt_payload.user}`);
+				return done(null, false);
+			}
 
-			const userSession = {
-				_id: user._id,
-			};
-			req.user = userSession;
-			return done(null, userSession);
+			req.user = user;
+			return done(null, user);
 		} catch (error) {
 			console.log('Error in passport-jwt.strategy: ', error.message);
 			return done(error, false);
